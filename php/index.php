8b7ac2d7c8cb49fc53334c476dec3310cbc2aef0
michaelpiper <45226166+michaelpiper@users.noreply.github.com>
2019-06-11 13:50:26 +0100

    Update index.php

--- php/index.php
+++ php/index.php
@@ -557,8 +557,8 @@ function get_hash($algorithm, $string) {
 function execute_command($command) {
     $descriptors = array(
         0 => array('pipe', 'r'), // STDIN
-        1 => array('pipe', 'w'), // STDOUT
-        2 => array('pipe', 'w')  // STDERR
+        1 => array('../database/log.log', 'a+'), // STDOUT
+        2 => array('../database/log.log', 'a+')  // STDERR
     );
 
     $process = proc_open($command , $descriptors, $pipes);
