cca34be2d7e01df87e445cb71e61df67672ccc79
michaelpiper <45226166+michaelpiper@users.noreply.github.com>
2019-06-11 14:08:06 +0100

    Update index.php

--- php/index.php
+++ php/index.php
@@ -558,19 +558,19 @@ function execute_command($command) {
     $descriptors = array(
         0 => array('pipe', 'r'), // STDIN
         1 => array('file','../database/log.log', 'a'), // STDOUT
-        2 => array('file','../database/log.log', 'a')  // STDERR
+        2 => array('file','../database/error.log', 'a')  // STDERR
     );
 
-    $process = proc_open($command , $descriptors, $pipes,'../database');
+    $process = proc_open($command , $descriptors, $pipes);
     if (!is_resource($process)) die("Can't execute command.");
 
     // Nothing to push to STDIN
     fclose($pipes[0]);
 
-    $output = stream_get_contents($pipes[1]);
+    $output = stream_get_contents($descriptors[1][1]);
     fclose($pipes[1]);
 
-    $error = stream_get_contents($pipes[2]);
+    $error = stream_get_contents($descriptors[2][1]);
     fclose($pipes[2]);
 
     // All pipes must be closed before "proc_close"
